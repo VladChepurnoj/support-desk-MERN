0556d8adbd8bc0b6bb92da9e61d39807e7107a9d
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-08-10 11:31:36 +0300

    First commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Welcome to the support desk API" });
+});
+
+//routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
