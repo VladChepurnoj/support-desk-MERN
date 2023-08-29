cb29444e896d93b2673e86babd142d014693337d
Vlad Chepurnyi <chepurnojv@gmail.com>
2023-08-29 15:13:55 +0300

    ticket form and ticket slice, ticket functionality and fetch them from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import PrivateRoute from './components/PrivateRoute'
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import NewTicket from './pages/NewTicket'
+import NewTicket from "./pages/NewTicket";
+import Tickets from "./pages/Tickets";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -18,9 +19,12 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/register" element={<Register />} />
-						<Route path='/new-ticket' element={<PrivateRoute />}>
-						<Route path='/new-ticket' element={<NewTicket />} />
-					  </Route>
+						<Route path="/new-ticket" element={<PrivateRoute />}>
+							<Route path="/new-ticket" element={<NewTicket />} />
+						</Route>
+						<Route path="/tickets" element={<PrivateRoute />}>
+							<Route path="/tickets" element={<Tickets />} />
+						</Route>
 					</Routes>
 				</div>
 			</Router>
